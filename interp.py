b88cc8b4dca8458438f88af655e91b838e9f3942
Dora F <doradora.prog@gmail.com>
2019-11-27 19:10:26 +0900

    First commit

---
+++ interp.py
@@ -0,0 +1,49 @@
+import sys
+
+ptr = 0
+memory = [0]
+readcnt = 0
+whilemark = []
+
+if len(sys.argv) == 2:
+    fname = sys.argv[1]
+    with open(fname) as f:
+        program = f.read()
+    while readcnt < len(program):
+        c = program[readcnt]
+        if c == ">":
+            if ptr < 1000000:
+                ptr += 1
+                while len(memory) <= ptr:
+                    memory.append(0)
+            else:
+                raise Exception("ptr is too large")
+        elif c == "<":
+            if ptr > 0:
+                ptr -= 1
+            else:
+                raise Exception("ptr must be positive value")
+        elif c == "+":
+            memory[ptr] += 1
+        elif c == "-":
+            memory[ptr] -= 1
+        elif c == ".":
+            print(chr(memory[ptr]))
+        elif c == "[":
+            if memory[ptr] == 0:
+                wcnt = 1
+                readcnt += 1
+                while wcnt > 0:
+                    if program[readcnt] == "[":
+                        wcnt += 1
+                    elif program[readcnt] == "]":
+                        wcnt -= 1
+                    readcnt += 1
+            else:
+                whilemark.append(readcnt)
+        elif c == "]":
+            if memory[ptr] != 0:
+                readcnt = whilemark[-1]
+            else:
+                whilemark.pop(len(whilemark)-1)
+        readcnt += 1
