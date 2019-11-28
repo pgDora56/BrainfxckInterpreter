f6ce322b2be6ddba2eb5f8f2e3417bfc76020769
Dora F <doradora.prog@gmail.com>
2019-11-28 14:41:51 +0900

    Add +/-/./>/< to hs-interpreter

--- hs/interp.hs
+++ hs/interp.hs
@@ -3,27 +3,43 @@
 import System.IO
 import System.Environment (getArgs)
 import Data.Char (chr)
-import qualified Data.ByteString.Char8 as DB
 
 main :: IO()
 main = do
     args <- getArgs
     withFile (args !! 0) ReadMode $ \handle -> do
-        program <- DB.hGetContents handle
+        program <- hGetContents handle
         print program
         interpret program (repeat 0) 0 0 
 
-interpret :: DB.ByteString -> [Int] -> Int -> Int -> IO ()
+main' :: IO()
+main' = do
+    file <- getLine
+    withFile file ReadMode $ \handle -> do
+        program <- hGetContents handle
+        print program
+        interpret program (repeat 0) 0 0 
+
+interpret :: String -> [Int] -> Int -> Int -> IO ()
 interpret prg memory idx rpos
-    | rpos >= (DB.length prg) = return ()
-    | (prg !! rpos) == "+" = do
-        nmemo <- increment memory idx
-        interpret prg nmemo idx (rpos+1)
-    | (prg !! rpos) == "." = do 
-        putStr $ chr (memory !! idx)
+    | rpos >= (length prg) = putChar '\n'
+    | ch == '>' = interpret prg memory (idx+1) (rpos+1)
+    | ch == '<' = if idx > 0 
+                    then interpret prg memory (idx-1) (rpos+1)
+                    else putStrLn "\nCan't allocate memory!"
+    | ch == '+' = interpret prg (increment memory idx) idx (rpos+1)
+    | ch == '-' = interpret prg (decrement memory idx) idx (rpos+1)
+    | ch == '.' = do 
+        putChar $ chr (memory !! idx)
         interpret prg memory idx (rpos+1)
+    | otherwise = interpret prg memory idx (rpos+1)
+        where ch = prg !! rpos
 
 increment :: [Int] -> Int -> [Int]
 increment lis x = f ++ [l+1] ++ ls
+    where (f, (l:ls)) = splitAt x lis
+
+decrement :: [Int] -> Int -> [Int]
+decrement lis x = f ++ [l-1] ++ ls
     where (f, (l:ls)) = splitAt x lis
 
