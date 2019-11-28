9c44e52daec80f03bdac087c5f648bac269024e8
Dora F <doradora.prog@gmail.com>
2019-11-28 12:08:43 +0900

    interpret-def OK

--- hs/interp.hs
+++ hs/interp.hs
@@ -1,13 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import System.IO
 import System.Environment (getArgs)
+import Data.Char (chr)
+import qualified Data.ByteString.Char8 as DB
 
 main :: IO()
 main = do
     args <- getArgs
     withFile (args !! 0) ReadMode $ \handle -> do
-        program <- hGetContents handle
-        putStrLn program
-        interpret program
+        program <- DB.hGetContents handle
+        print program
+        interpret program (repeat 0) 0
 
-interpret :: String -> ()
-interpret program =
+interpret :: DB.ByteString -> [Int] -> Int -> IO ()
+interpret "" _ _ = return ()
+interpret (p:prg) memory idx 
+    | p == "+" = do
+        nmemo <- increment  memory idx
+        interpret prg nmemo idx
+    | p == "." = do 
+        putStr $ chr (memory !! idx)
+        interpret prg memory idx
+
+increment :: [Int] -> Int -> [Int]
+increment lis x = f ++ [l+1] ++ ls
+    where (f, (l:ls)) = splitAt x lis
+
