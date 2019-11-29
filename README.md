db2d13dc58a14dc891b09a048adbf6c3ecf4c7be
Dora F <doradora.prog@gmail.com>
2019-11-29 11:37:51 +0900

    hs complete

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Interpreter of Brainfxck and its variants
 
+Input isn't implemented.
+
 ## Files
 * /python => Brainfxck Interpreter made by Python3
 * /haskell => Brainfxck Interpreter made by Haskell(incomplete)
--- hs/interp.hs
+++ hs/interp.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Debug.Trace
 import System.IO
 import System.Environment (getArgs)
 import Data.Char (chr)
@@ -9,30 +10,36 @@ main = do
     args <- getArgs
     withFile (args !! 0) ReadMode $ \handle -> do
         program <- hGetContents handle
-        print program
-        interpret program (repeat 0) 0 0 
+        interpret program (repeat 0) [] 0 0 
 
+-- For Debug in GHCi
 main' :: IO()
 main' = do
     file <- getLine
     withFile file ReadMode $ \handle -> do
         program <- hGetContents handle
         print program
-        interpret program (repeat 0) 0 0 
+        interpret program (repeat 0) [] 0 0 
 
-interpret :: String -> [Int] -> Int -> Int -> IO ()
-interpret prg memory idx rpos
+interpret :: String -> [Int] -> [Int] -> Int -> Int -> IO ()
+interpret prg memory while idx rpos
     | rpos >= (length prg) = putChar '\n'
-    | ch == '>' = interpret prg memory (idx+1) (rpos+1)
+    | ch == '>' = interpret prg memory while (idx+1) (rpos+1)
     | ch == '<' = if idx > 0 
-                    then interpret prg memory (idx-1) (rpos+1)
+                    then interpret prg memory while (idx-1) (rpos+1)
                     else putStrLn "\nCan't allocate memory!"
-    | ch == '+' = interpret prg (increment memory idx) idx (rpos+1)
-    | ch == '-' = interpret prg (decrement memory idx) idx (rpos+1)
+    | ch == '+' = interpret prg (increment memory idx) while idx (rpos+1)
+    | ch == '-' = interpret prg (decrement memory idx) while idx (rpos+1)
+    | ch == '[' = interpret prg memory ((rpos+1):while) idx (rpos+1)
+    | ch == ']' = if (length while) > 0 then
+                    if (memory !! idx) == 0
+                        then interpret prg memory (tail while) idx (rpos+1)
+                        else interpret prg memory while idx (head while)
+                  else putStrLn "\n[ isn't found!"
     | ch == '.' = do 
         putChar $ chr (memory !! idx)
-        interpret prg memory idx (rpos+1)
-    | otherwise = interpret prg memory idx (rpos+1)
+        interpret prg memory while idx (rpos+1)
+    | otherwise = interpret prg memory while idx (rpos+1)
         where ch = prg !! rpos
 
 increment :: [Int] -> Int -> [Int]
